01065eba9eedafb869f6e430bf2964ffbc8bea90
ryanthornton628 <ryanthornton628@gmail.com>
2019-07-24 19:36:47 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,59 +1,61 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  i = 0 
-  key = String
-  while i < cart.length do 
-    (cart[i])[key][:count] = 1
-    i = i + 1  
+  i = 0
+  c = [] 
+  
+  cart.each do |item|
+    c.push(item.to_a)
   end
-  while i < cart.length do 
-    if cart.count(cart[i]) > 1
-      (cart[i])[key][:count] = cart.count(cart[i])
-    end
-    i = i + 1
-  end
- cart = cart.uniq
- c_cart = []
- cart.map do |item|
-    c_cart.push(item.to_a)
-  end 
   result = {}
-  while i < c_cart.length do
-    result = {
-      c_cart[i][1] => c_cart[i][2]
-    }
-    i = i + 1 
+  
+  c.each do |item|
+    item[0][1][:count] = c.count(item)
+  end
+  
+  
+  c = c.uniq 
+  
+  c.each do |item|
+    result[item[0][0]] = item[0][1]
   end
   return result
-  
 end
-
-
-
-  #while i < cart.length do
-   # if cart.count(cart[i]) > 1 
-    #  (cart[i])[key][:count] = cart.count(cart[i])
-     # i = i + 1 
-    #elsif cart.count(cart[i]) = 1 
-    #(cart[i])[key][:count] = 1 
-    #i =  i + 1 
-    #end
-  #end
-  #cart_revised = cart.uniq; 
-  
-  #cart_revised.map do |item|
-   # item = item.to_a
-  #end
-  #while i < cart_revised.length do
-   # c_cart[(cart_revised[i])[i]] = (cart_revised[i])[i + 1]
-    #i = i + 1 
-  #end
-  #return c_cart 
-#end
-  
 
 def apply_coupons(cart, coupons)
-  # code here
+  result = {}
+  coupons.each do |coupon|
+    item = coupon[:item]
+    cost = coupon[:cost]
+    num = coupon[:num]
+    if cart.has_key?(item) && cart[item][:count] >= num
+      cart["#{item} W/COUPON"] = {
+        :price => (cost)/(num),
+        :clearance => cart[item][:clearance],
+        :count => num
+        }
+    end
+    if cart.has_key?(item) && cart.has_key?("#{item} W/COUPON")
+        cart[item][:count] = cart[item][:count] - item[:num]
+        if cart[item][:count] == 0
+          cart.delete(item)
+        end
+    end
+  end
+  return cart 
 end
+
+  
+  
+  
+  #coupons_array.each do |items|
+   # cart["#{items[0][1]} W/COUPON"] = {
+   #:price => items[2][1]/items[1][1]
+    #:clearance => cart[items[0][1]][:clearance]
+    #:count => items[1][1]
+    #}
+  #end
+  #return cart
 
 def apply_clearance(cart)
   # code here
