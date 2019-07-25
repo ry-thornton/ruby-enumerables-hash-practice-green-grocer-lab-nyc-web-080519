182af74a5e27b7fc5d7acaefe1919a972033ffa0
ryanthornton628 <ryanthornton628@gmail.com>
2019-07-25 15:17:48 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -3,64 +3,68 @@ require 'pry'
 def consolidate_cart(cart)
   i = 0
   c = [] 
-  
   cart.each do |item|
     c.push(item.to_a)
   end
-  result = {}
-  
+  cc = {}
   c.each do |item|
     item[0][1][:count] = c.count(item)
   end
-  
-  
   c = c.uniq 
   
   c.each do |item|
-    result[item[0][0]] = item[0][1]
+    cc[item[0][0]] = item[0][1]
   end
-  return result
+  return cc
 end
 
 def apply_coupons(cart, coupons)
-  result = {}
   coupons.each do |coupon|
-    item = coupon[:item]
-    cost = coupon[:cost]
-    num = coupon[:num]
-    if cart.has_key?(item) && cart[item][:count] >= num
-      cart["#{item} W/COUPON"] = {
-        :price => (cost)/(num),
-        :clearance => cart[item][:clearance],
-        :count => num
-        }
+      item = coupon[:item]
+      cost = coupon[:cost]
+      num = coupon[:num]
+      if cart.has_key?(item) && cart[item][:count] >= num
+        if !cart.has_key?("#{item} W/COUPON")
+          cart["#{item} W/COUPON"] = {
+            :price => (cost)/(num),
+            :clearance => cart[item][:clearance],
+            :count => num
+          }
+          elsif cart.has_key?("#{item} W/COUPON")
+          cart["#{item} W/COUPON"][:count] = cart["#{item} W/COUPON"][:count] + num
+          
+          end
+        cart[item][:count] = cart[item][:count] - num
+      end
+      
     end
-    if cart.has_key?(item) && cart.has_key?("#{item} W/COUPON")
-        cart[item][:count] = cart[item][:count] - item[:num]
-        if cart[item][:count] == 0
-          cart.delete(item)
-        end
-    end
-  end
   return cart 
+  
 end
 
-  
-  
-  
-  #coupons_array.each do |items|
-   # cart["#{items[0][1]} W/COUPON"] = {
-   #:price => items[2][1]/items[1][1]
-    #:clearance => cart[items[0][1]][:clearance]
-    #:count => items[1][1]
-    #}
-  #end
-  #return cart
-
 def apply_clearance(cart)
-  # code here
+  cart.each do |k, v|
+    if v[:clearance] == TRUE
+      price = v[:price]-(v[:price]*0.2)
+      v[:price] = price.round(1)
+    end
+  end
 end
 
 def checkout(cart, coupons)
-  # code here
+  sum = 0
+  consolidate_cart(cart)
+  apply_coupons(cc, coupons)
+  apply_clearance(cc)
+  total = []
+  cc.each do |k, v|
+    total.push(v[:price]*v[:count])
+  end
+  total.each do |num|
+    sum = sum + num
+  end
+  if sum > 100
+     return sum - sum*0.1
+  end
+  return sum 
 end
